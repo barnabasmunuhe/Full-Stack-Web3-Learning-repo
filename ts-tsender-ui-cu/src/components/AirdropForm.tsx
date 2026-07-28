350403e6f236d43e38f0a5a19a9daf979267906b
barnabas <barnabasmunuhe@gmail.com>
2026-07-28 15:18:26 +0300

    feat: Modified AirdropForm.tsx & InputField.tsx

--- ts-tsender-ui-cu/src/components/AirdropForm.tsx
+++ ts-tsender-ui-cu/src/components/AirdropForm.tsx
@@ -3,14 +3,40 @@ import InputField from "@/components/ui/InputField"
 import { useState } from "react"
 
 export default function AirdropForm() {
+    const [tokenAddress, setTokenAddress] = useState("")
+    const [recipientAddresses, setRecipientAddresses] = useState("")
+    const [amounts, setAmounts] = useState("")
+
+    async function handleSubmit() {
+        console.log(tokenAddress, recipientAddresses, amounts);
+    }
+
     return (
         <div>
             <InputField
                 label="Token Address"
-                placeholder="Enter the token address"
+                placeholder="0x"
                 value={tokenAddress}
                 onChange={(e) => setTokenAddress(e.target.value)}
             />
+            <InputField
+                label="Recipient Address (comma or new line separated)"
+                placeholder="0x123..., 0x456..."
+                value={recipientAddresses}
+                onChange={(e) => setRecipientAddresses(e.target.value)}
+                large={true}
+            />
+            <InputField
+                label="Amounts (wei; comma or new line separated)"
+                placeholder="100,200,300,..."
+                value={amounts}
+                onChange={(e) => setAmounts(e.target.value)}
+                large={true}
+            />
+
+            <button onClick={handleSubmit}>
+                Send Tokens
+            </button>
         </div>
     )
 }
--- ts-tsender-ui-cu/src/components/ui/InputField.tsx
+++ ts-tsender-ui-cu/src/components/ui/InputField.tsx
@@ -1,29 +1,31 @@
-type InputFieldProps = {
+type InputFormProps = {
   label: string;
   placeholder: string;
   value: string;
   type?: "input" | "textarea";
+  large?: boolean;
   onChange: (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => void;
 };
 
-export default function InputField({
+export default function InputForm({
   label,
   placeholder,
   value,
-  type = "input",
+  type,
+  large,  
   onChange,
-}: InputFieldProps) {
+}: InputFormProps) {
   return (
     <div className="flex flex-col gap-2 w-full">
       <label className="font-medium text-sm text-gray-700">
         {label}
       </label>
 
-      {type === "textarea" ? (
+      {large ? (
         <textarea
-          className="w-full rounded-lg border border-gray-300 p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="`bg-white py-2 px-3 border border-zinc-300 placeholder:text-zinc-500 text-zinc-900 shadow-xs rounded-lg focus:ring-[4px] focus:ring-zinc-400/15 focus:outline-none h-24 align-text-top`"
           placeholder={placeholder}
           value={value}
           onChange={onChange}
@@ -31,8 +33,8 @@ export default function InputField({
         />
       ) : (
         <input
-          className="w-full rounded-lg border border-gray-300 p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          type="text"
+          className={"bg-white py-2 px-3 border border-zinc-300 placeholder:text-zinc-500 text-zinc-900 shadow-xs rounded-lg focus:ring-[4px] focus:ring-zinc-400/15 focus:outline-none"}
+          type={type}
           placeholder={placeholder}
           value={value}
           onChange={onChange}
