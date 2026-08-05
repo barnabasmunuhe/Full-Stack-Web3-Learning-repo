6cb2999f0cd2cf3c7872cff0d769cfc30eeca0f7
barnabas <barnabasmunuhe@gmail.com>
2026-08-05 14:28:29 +0300

    feat: adding comments in; rainbowKitConfig.tsx

--- ts-tsender-ui-cu/src/rainbowKitConfig.tsx
+++ ts-tsender-ui-cu/src/rainbowKitConfig.tsx
@@ -1,4 +1,5 @@
-"use client"
+//Tells us what kind of chains & wallets we can connect to & how to connect to our app.
+"use client"// Tells the react component to be rendered on the client side, not the server side. This is important for things like wallet connections that require user interaction.
 
 import { getDefaultConfig} from "@rainbow-me/rainbowkit"
 import {anvil,zksync,mainnet,sepolia} from "wagmi/chains"
