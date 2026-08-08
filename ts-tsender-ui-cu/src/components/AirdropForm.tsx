db7e02eb884026b0960591b9e83584e886f3acb4
barnabas <barnabasmunuhe@gmail.com>
2026-08-08 07:59:25 +0300

    feat: Imported a anvil instance with the tsender contract deployed there. feat: Added another script in package.json anvil --load-state tsender-deployed.json feat:Created a constants.ts with the different tsender addresses on different chains(chainToTsender), erc20Abi & tsenderContractAbi feat: Designed & implemented a function to getApprovedAmount/ reading the allowance function on the erc20Abi feat: Imported a mock token (MT)

--- ts-tsender-ui-cu/src/components/AirdropForm.tsx
+++ ts-tsender-ui-cu/src/components/AirdropForm.tsx
@@ -1,14 +1,44 @@
 "use client"
 import InputField from "@/components/ui/InputField"
 import { useState } from "react"
+import {chainsToTSender, tsenderAbi, erc20Abi} from "@/constants"
+import { useChainId, useConfig, useAccount} from 'wagmi'
+import { readContract } from '@wagmi/core'
 
 export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("")
     const [recipientAddresses, setRecipientAddresses] = useState("")
     const [amounts, setAmounts] = useState("")
+    const chainId = useChainId()
+    const config = useConfig()
+    const account = useAccount()
+
+    async function getApprovedAmount(tsenderContractAddress: string | null) : Promise<number> {
+        if(!tsenderContractAddress) {
+            alert("No address found, Please use a supported chain")
+            return 0
+        }
+        // read from the chain to see we have approved enough tokens
+        // allowance (it's a read function)
+        // instead of using the useReadContract hook, we will use the readContract function from wagmi to read the allowance of the token for the Tsender contract
+        const allowanceResponse = await readContract( config, {
+            abi: erc20Abi,
+            address: tokenAddress as `0x${string}`, //received from our tokenAddress state hook
+            functionName: "allowance",
+            args: [account.address, tsenderContractAddress as `0x${string}`]
+        })
+        // same as in solidity: allowance(account, tsenderContractAddress)
+        return allowanceResponse as number
+    } 
 
     async function handleSubmit() {
-        console.log(tokenAddress, recipientAddresses, amounts);
+        // 1a. If approved move to step 2
+        // 1b. Otherwise approve the Tsender contract to send our tokens
+        // 2. Call the airdrop function on Tsender contract
+        // 3. Wait for the transaction to be confirmed/mined
+
+        const tSenderContractAddress = chainsToTSender[chainId]["tsender"]
+        const approvedAmount = await getApprovedAmount(tSenderContractAddress)// will get how much is Aprroved
     }
 
     return (
@@ -33,8 +63,15 @@ export default function AirdropForm() {
                 onChange={(e) => setAmounts(e.target.value)}
                 large={true}
             />
-
-            <button onClick={handleSubmit}>
+            <button onClick={handleSubmit} className="
+            px-6 py-3
+            bg-blue-600 hover:bg-blue-700
+            text-white font-semibold 
+            rounded-lg 
+            shadow-sm
+            transition-colors duration-200
+            focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2
+            disabled:opacity-50 disabled:cursor-not-allowed">
                 Send Tokens
             </button>
         </div>
