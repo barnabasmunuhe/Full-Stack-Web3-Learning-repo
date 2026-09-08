b52d62a0badd0071641e04a87f69d1f4ccc971dc
barnabas <barnabasmunuhe@gmail.com>
2026-09-08 08:14:46 +0300

    Prepare frontend for Vercel deployment

--- ts-tsender-ui-cu/next.config.ts
+++ ts-tsender-ui-cu/next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  output: 'export', // Tells Next.js not to expect a Node.js server and to export the app as static app
+  distDir: 'out',
+  images: {
+    unoptimized: true, // Tells Next.js not to optimize images and to use the original image files as-is
+  },
+  basePath: "",
+  assetPrefix: "./",
+  trailingSlash: true,
 };
 
 export default nextConfig;
